ac90b7c1af8773942eee745cfbb726b9296f1657
Lera06 <lera06@rambler.ru>
2023-06-07 01:21:05 +0300

    Commit: Added Models (Product, Category, Discount)

--- shop/urls.py
+++ shop/urls.py
@@ -5,6 +5,8 @@ app_name = 'shop'
 
 urlpatterns = [
     path('', ShopView.as_view(), name='shop'),
-    path('product/<int:id>/', ProductSingleView.as_view(), name='product'),
+    path('product/<int:product_id>/', ProductSingleView.as_view(), name='product'),
+    path('product/', ProductSingleView.as_view(), name='product'),
     path('cart/', CartView.as_view(), name='cart'),
     ]
+
