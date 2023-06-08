ffd931d3caba6949386b8124a1c34fadbd37edf6
Lera06 <lera06@rambler.ru>
2023-06-08 10:11:49 +0300

    Commit: Added Pagination

--- shop/views.py
+++ shop/views.py
@@ -2,13 +2,41 @@ from django.shortcuts import render
 from django.views import View
 from django.db.models import OuterRef, Subquery, F, ExpressionWrapper, DecimalField
 from .models import Product, Discount
+from django.core.paginator import Paginator
 
 
 # Create your views here.
+# Функцмя для отображения страниц
+def get_pages_list(page, max_pages):
+    if max_pages < 5:
+        return list(range(1, max_pages + 1))
+
+    data = [1]
+    if page > 3:
+        data += ["..."]
+
+    if 2 < page < max_pages - 1:
+        data += list(range(page - 1, page + 2))
+    elif page == 1:
+        data += [2]
+    elif page == 2:
+        data += [2, 3]
+    elif page == max_pages - 1:
+        data += [page - 1, page]
+    elif page == max_pages:
+        data += [page - 1]
+
+    if page + 2 < max_pages:
+        data += ["..."]
+    data += [max_pages]
+
+    return data
+
 
 class ShopView(View):
 
     def get(self, request):
+        items_per_page = 2
 
         price_with_discount = ExpressionWrapper(
             F('price') * (100.0 - F('discount_value')) / 100.0,
@@ -30,7 +58,23 @@ class ShopView(View):
         if not category == "All":
             products = products.filter(category__category_name=category)  # имя модели__имя поля
 
-        return render(request, 'shop/shop.html', {"data": products, "category": category})
+
+        paginator = Paginator(products, items_per_page)
+        page = int(request.GET.get('page', 1))
+        items = paginator.get_page(page)
+
+        max_pages = paginator.num_pages
+        data_pages = get_pages_list(page, max_pages)
+
+        return render(request, 'shop/shop.html',
+                      {"data": items,
+                       "category": category,
+                       "page": page,
+                       "next": items.has_next(),
+                       "previous": items.has_previous(),
+                       "max_pages": max_pages,
+                       "data_pages": data_pages})
+
 
         # context = {'data': [{'name': 'Bell Pepper',
         #                      'discount': 30,
